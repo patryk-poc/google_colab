3f8527f9a195989d82a1cedc1ec3299e642cb487
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2023-02-17 17:48:13 +0100

    tests(web): Add chaining method calls

--- tests/unit/test_web.py
+++ tests/unit/test_web.py
@@ -17,3 +17,13 @@ def test_get_ip():
         ip = client.get_ip()
         assert ip.text == "1.2.3.4"
         mock_get.assert_called_once_with("https://httpbin.org/ip")
+
+
+def test_get_ip_chaining_method_calls():
+    mock_response = Mock(text="1.2.3.4")
+    mock_session = Mock(get=Mock(return_value=mock_response))
+
+    with patch.object(Web, "configure_session", return_value=mock_session):
+        ip = Web().get_ip()
+        assert ip.text == "1.2.3.4"
+        mock_session.get.assert_called_once_with("https://httpbin.org/ip")
